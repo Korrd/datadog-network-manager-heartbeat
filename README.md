4466bc2ca935e9b62d638a5cba428431b95005ba
Korrd <lordkorrd@gmail.com>
2022-07-09 11:15:58 +0200

    Add heartbeat script draft

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# datadog-network-manager-heartbeat
-Sends ok/not ok metrics depending on linux network-manager state
+## About this script
+
+This custom datadog event checks if our home server has a private IP address. If it's not there, it restarts the network-manager service and emits a datadog event.
+
+A niche fix created to tackle a pesky issue which I've been unable to solve otherwise: ubuntu's network-manager dies at random and kills network connectivity due to its temporary wifi card having a crap driver (which will soon be replaced by trusty, UTP cable) which dies every now and then and need a kick in the butt™.
+
+Created by Victor M. Martin - 2022-07-09, while conquering Spain 🇪🇸 and bitching with his home server ~20.000 leagues away.
+
+## Requirements
+
+- Python 3
+- Meant for `Ubuntu 20.04` or any ubuntu distro using network-manager
+- Some python libraries, see [the requirements file](./requirements.txt)
+  ```bash
+    pip3 install -r requirements.txt
+  ```
+
+## Usage
+
+Get datadog app and api keys from here: https://app.datadoghq.com/organization-settings/users, then replace placeholders with app and api keys, and the machine's expected IP address (note that `--debug` flag is optional: all it does it to print stuff to stdout for ease of debugging)
+
+```bash
+  check.py --app-key=<datadog_app_key> --api-key=<datadog_api_key> --target-ip=<server_private_ip> [--debug]
+```
+
+## Notes
+
+This script requires root privileges, otherwise it'll be unable to restart the network-manager service.
---
+++ check.py
@@ -0,0 +1,72 @@
+from datadog import initialize, api
+from sys import argv as args
+import netifaces
+import subprocess
+
+FLAG_DEBUG = ("--debug" in args)  # for debugging purposes
+
+def main(args):
+
+  # Get params and initialize datadog
+  APP_KEY = getArgumentValue(args=args, flag="--app-key")
+  API_KEY = getArgumentValue(args=args, flag="--api-key")
+  EXPECTED_IP = getArgumentValue(args=args, flag="--target-ip")
+
+  options = {
+    "api_key": API_KEY,
+    "app_key": APP_KEY,
+  }
+  initialize(**options)
+
+  EVENT_TITLE = f"Network manager status at '{EXPECTED_IP}'"
+  # Determine if network-manager is disconnected
+
+  # Send event to datadog
+  if not hasExpectedIpAddress(hostIp=EXPECTED_IP):
+    restartNetworkManager()
+    sendDatadogEvent(title=EVENT_TITLE, text=f"'{EXPECTED_IP}' got its network-manager restarted", tags=["host:'{EXPECTED_IP}'"])
+    if FLAG_DEBUG:
+      print("🐶 Sending restart event to datadog...")
+  else:
+    if FLAG_DEBUG:
+      print ("✅ All is well with the server")
+
+## ============================================================================
+## Functions ==================================================================
+## ============================================================================
+
+### Sends an event to datadog
+def sendDatadogEvent(title, text, tags):
+
+  api.Event.create(title=title, text=text, tags=tags)
+
+### Gets an argument's value based on its flag and separator
+def getArgumentValue(args, flag, separator="="):
+    for element in args:
+        if element.startswith(flag):
+            return element.split(separator)[1]
+    return ""  # Return empty if not found
+
+### This function checks if the host machine got assigned the expected IP addr
+### we don't care about more complex checks as the sever's IP is fixed
+def hasExpectedIpAddress(hostIp):
+
+  for iface in netifaces.interfaces():
+    iface_details = netifaces.ifaddresses(iface)
+    if netifaces.AF_INET in iface_details:
+      ipAddress = iface_details[netifaces.AF_INET][0].get("addr")
+      if ipAddress == hostIp:
+        return True
+  return False
+
+def restartNetworkManager():
+  if FLAG_DEBUG:
+    print("❌ Restarting network-manager...")
+  command = "service restart network-manager"   # might be wlan0
+  proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+
+## ============================================================================
+## Entrypoint --- ALWAYS PLACE AT EOF =========================================
+## ============================================================================
+if __name__ == "__main__":
+    main(args)
